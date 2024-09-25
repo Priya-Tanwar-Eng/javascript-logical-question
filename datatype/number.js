e0f168b6ad3e1469f2a7537454b0ca047da5ece7
Priya-Tanwar-Eng <priya8261717@gmail.com>
2024-09-25 23:33:19 +0530

    data-type

---
+++ datatype/number.js
@@ -0,0 +1,18 @@
+let a = 4;
+console.log(typeof a);
+let b = "hello";
+console.log(typeof b);
+let c = true;
+console.log(typeof c)
+let d = {
+    name:"priya",
+    age:24,
+}
+console.log(typeof d)
+let e = [1,2,3,4];
+console.log(typeof e);
+
+let f;
+console.log(typeof f);
+let g = null;
+console.log(typeof g)
