b6bea9c962601599628598d13e96d541532bba36
Priya-Tanwar-Eng <priya8261717@gmail.com>
2024-09-25 23:58:58 +0530

    operators

---
+++ arithmetic.js
@@ -0,0 +1,13 @@
+//arithmetic operators
+
+let a =5, b=4;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+console.log(a++);
+console.log(++a);
+console.log(a--);
+console.log(--a);
---
+++ condition.js
@@ -0,0 +1,11 @@
+let a=5;
+let b=5;
+let c=6;
+console.log(a==b);
+console.log(a===b);
+console.log(a!=b);
+console.log(a!==b);
+console.log(a>b);
+console.log(a<b);
+console.log(a>=b);
+console.log(a<=b);
